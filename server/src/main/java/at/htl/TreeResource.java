1f02fbded19a7d2b7e83eada60f01c0b33d26861
Jonas Dorfinger <contact@dorfingerjonas.at>
2021-11-29 14:06:37 +0100

    fix getTreesOfType()

--- server/src/main/java/at/htl/TreeResource.java
+++ server/src/main/java/at/htl/TreeResource.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Path("/tree")
 @Produces(MediaType.APPLICATION_JSON)
