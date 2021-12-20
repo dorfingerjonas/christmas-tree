c57903d2d791508058946b6bb88a3ca22221525e
Jonas Dorfinger <contact@dorfingerjonas.at>
2021-12-20 12:57:39 +0100

    implement tree type selection

---
+++ client/src/app/tree/type-selection/type-selection.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { BackendService, TreeType } from '../../../shared/backend.service';
+
+@Component({
+  selector: 'app-type-selection',
+  templateUrl: './type-selection.component.html',
+  styleUrls: [ './type-selection.component.scss' ]
+})
+export class TypeSelectionComponent implements OnInit {
+
+  public treeTypes: TreeType[] | null;
+
+  constructor(private readonly backend: BackendService) {
+    this.treeTypes = null;
+  }
+
+  public ngOnInit(): void {
+    this.backend.getAllTreeTypes()
+      .subscribe({
+        next: res => {
+          this.treeTypes = res;
+        },
+        error: err => {
+          console.log(err.messageText);
+        }
+      });
+  }
+
+}
