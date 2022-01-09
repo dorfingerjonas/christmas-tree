45c4f4c34a6ffc7b2b4ed4e60aeae92ad1ee9d20
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-01-09 20:21:09 +0100

    update tree display component

--- client/src/app/tree/tree-display/tree-display.component.ts
+++ client/src/app/tree/tree-display/tree-display.component.ts
@@ -1,31 +1,40 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {BackendService, ITree} from "../../shared/backend.service";
+import { Component, Input, OnInit } from '@angular/core';
+import { BackendService, ITree } from '../../shared/backend.service';
+import { Router } from '@angular/router';
+import { SellDataService } from '../../shared/sell-data.service';
 
 @Component({
-    selector: 'app-tree-display',
-    templateUrl: './tree-display.component.html',
-    styleUrls: ['./tree-display.component.scss']
+  selector: 'app-tree-display',
+  templateUrl: './tree-display.component.html',
+  styleUrls: [ './tree-display.component.scss' ]
 })
 export class TreeDisplayComponent implements OnInit {
 
-    @Input()
-    public treeId!: number;
+  @Input()
+  public treeId!: number;
 
-    @Input()
-    public type!: string;
+  @Input()
+  public type!: string;
 
-    public tree: ITree | null;
+  public tree: ITree | null;
 
-    public constructor(private readonly backendService: BackendService) {
-        this.tree = null;
-    }
+  public constructor(private readonly backendService: BackendService,
+                     private readonly sell: SellDataService,
+                     private readonly router: Router) {
+    this.tree = null;
+  }
 
-    public async ngOnInit(): Promise<void> {
-        this.tree = await this.backendService.getTreeById(this.type, this.treeId);
-    }
+  public get btnText(): string {
+    return this.tree?.sold ? 'Sold' : 'Buy';
+  }
 
-    public get btnText(): string {
-        return this.tree?.sold ? 'Sold' : 'Buy';
-    }
+  public async ngOnInit(): Promise<void> {
+    this.tree = await this.backendService.getTreeById(this.type, this.treeId);
+  }
+
+  public buyTree(): void {
+    this.sell.tree = this.tree;
+    this.router.navigate([ 'sale' ]);
+  }
 
 }
