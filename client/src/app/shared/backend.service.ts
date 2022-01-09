65d7ee85235bce5f9799807bb7ab4d48d107f0fa
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-01-09 20:21:22 +0100

    update backend service

--- client/src/app/shared/backend.service.ts
+++ client/src/app/shared/backend.service.ts
@@ -1,38 +1,44 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {firstValueFrom, Observable} from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom, Observable } from 'rxjs';
 
-const BASE_URL: string = 'http://localhost:8080/tree'
+const BASE_URL: string = 'http://localhost:8080/tree';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class BackendService {
 
-    constructor(private readonly http: HttpClient) {
-    }
+  constructor(private readonly http: HttpClient) {
+  }
 
-    public getAllTreeTypes(): Observable<string[]> {
-        return this.http.get<string[]>(`${BASE_URL}/types`);
-    }
+  public getAllTreeTypes(): Observable<string[]> {
+    return this.http.get<string[]>(`${ BASE_URL }/types`);
+  }
 
-    getTreesOfType(type: string): Promise<number[]> {
-        const url = `${BASE_URL}/${type}`;
-        const resp = this.http.get<number[]>(url);
-        return firstValueFrom(resp);
-    }
+  getTreesOfType(type: string): Promise<number[]> {
+    const url = `${ BASE_URL }/${ type }`;
+    const resp = this.http.get<number[]>(url);
+    return firstValueFrom(resp);
+  }
 
-    public getTreeById(type: string, id: number): Promise<ITree> {
-        const url = `${BASE_URL}/${type}/${id}`;
-        const resp = this.http.get<ITree>(url);
-        return firstValueFrom(resp);
-    }
+  public getTreeById(type: string, id: number): Promise<ITree> {
+    const url = `${ BASE_URL }/${ type }/${ id }`;
+    const resp = this.http.get<ITree>(url);
+    return firstValueFrom(resp);
+  }
+
+  public buyTree(tree: ITree): Promise<ITree> {
+    const url = `${ BASE_URL }/${ tree.type }/${ tree.id }/buy`;
+    const resp = this.http.patch<ITree>(url, {});
+    return firstValueFrom(resp);
+  }
 }
 
 export interface ITree {
-    id: number,
-    type: string,
-    heightMeters: number,
-    price: number,
-    sold: boolean
+  id: number,
+  type: string,
+  heightMeters: number,
+  price: number,
+  sold: boolean
 }
